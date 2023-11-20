043df149d09924176d0582b2d26d78f06bfb1cc3
Andrés Montecinos Kern <andres.mkern@gmail.com>
2023-11-20 17:26:20 -0300

    ChatTerminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:mis_finanzas_app/presentation/screens/counter_functions_screen.dart';
-import 'package:mis_finanzas_app/presentation/screens/counter_screen.dart';
+import 'package:mis_finanzas_app/config/theme/app_theme.dart';
+import 'package:mis_finanzas_app/presentation/providers/chat_provider.dart';
+import 'package:mis_finanzas_app/presentation/screens/chat/chat_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.red,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().theme(),
+        home: const ChatScreen(),
       ),
-      home: const CounterFunctionsScreen(),
     );
   }
 }
